3aae7969867c30fbb2a50119a599546f3a20af80
denchik <surkovdanila43@gmail.com>
2024-11-30 12:11:30 +0400

    Чутка адаптивная верстка, оформил кнопки, в общем улучшение визуала главной страницы сайта, добавил прогрузку сайта с помощью js

---
+++ loader.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    
+    setTimeout(function() {
+        loader.classList.add('hidden');
+    }, 1000); 
+});
