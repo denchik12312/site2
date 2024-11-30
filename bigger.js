c95b2c1d7bd206025532b7c4e9e1c3d2b0f9c127
denchik <surkovdanila43@gmail.com>
2024-11-30 23:13:57 +0400

    сделана страница о нас

---
+++ bigger.js
@@ -0,0 +1,11 @@
+
+function make_him_big1(){
+    const aboutOne = document.getElementById('about-one');
+    aboutOne.classList.toggle('fullscreen');
+}
+
+
+function make_him_big2(){
+    const aboutOne = document.getElementById('about-two');
+    aboutOne.classList.toggle('fullscreen')
+}
